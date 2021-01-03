477a246629c51abdc2f10219c33945017d4d05e5
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2021-01-03 14:05:47 +0100

    feat: Se han añadido las clases CasillaMina50 y CasillaMinaReset
    Se han añadido e implementado las clases CasillaMina50 y CasillaMinaReset que heredan de la clase CasillaMina.
    
    Signed-off-by: Iker0610 <48362673+Iker0610@users.noreply.github.com>

--- src/main/java/is/buscaminas/model/buscaminas/casillas/CasillaMina.java
+++ src/main/java/is/buscaminas/model/buscaminas/casillas/CasillaMina.java
@@ -1,18 +1,20 @@
 package is.buscaminas.model.buscaminas.casillas;
 
+
 import java.beans.PropertyChangeListener;
 
-public class CasillaMina extends Casilla {
 
-    //Constructora
-    public CasillaMina (PropertyChangeListener pVistaCasilla)
-    {
-        super(pVistaCasilla);
-    }
-
-    public CasillaMina (CasillaTemp pCasilla)
-    {
-        //  Transforma la casilla temporal en una casilla mina
-        super(pCasilla);
-    }
+public class CasillaMina extends Casilla
+{
+	//Constructora
+	public CasillaMina(PropertyChangeListener pVistaCasilla)
+	{
+		super(pVistaCasilla);
+	}
+	
+	public CasillaMina(CasillaTemp pCasilla)
+	{
+		//  Transforma la casilla temporal en una casilla mina
+		super(pCasilla);
+	}
 }
---
+++ src/main/java/is/buscaminas/model/buscaminas/casillas/CasillaMina50.java
@@ -0,0 +1,19 @@
+package is.buscaminas.model.buscaminas.casillas;
+
+
+import java.beans.PropertyChangeListener;
+
+
+public class CasillaMina50 extends CasillaMina
+{
+	public CasillaMina50(PropertyChangeListener pVistaCasilla)
+	{
+		super(pVistaCasilla);
+	}
+	
+	public CasillaMina50(CasillaTemp pCasilla)
+	{
+		//  Transforma la casilla temporal en una casilla mina reset
+		super(pCasilla);
+	}
+}
