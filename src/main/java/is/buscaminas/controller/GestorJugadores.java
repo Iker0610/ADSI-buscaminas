d6d591f03dfa1f572088988d691506bde11f0783
IriaSanMiguel <48363337+IriaSanMiguel@users.noreply.github.com>
2021-01-15 23:31:25 +0100

    Feat: Implementación de guardar y eliminar

--- src/main/java/is/buscaminas/controller/GestorJugadores.java
+++ src/main/java/is/buscaminas/controller/GestorJugadores.java
@@ -7,6 +7,8 @@ import is.buscaminas.model.db.ResultadoSQL;
 import javafx.scene.control.Alert;
 
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GestorJugadores {
     //Atributos
@@ -14,9 +16,10 @@ public class GestorJugadores {
     private String usuarioSeleccionado;
 
     //Constructora
-    private GestorJugadores(){}
+    private GestorJugadores() {
+    }
 
-    public static GestorJugadores getGestorJugadores(){
+    public static GestorJugadores getGestorJugadores() {
         if (mGestorJugadores == null) mGestorJugadores = new GestorJugadores();
         return mGestorJugadores;
 
@@ -25,7 +28,7 @@ public class GestorJugadores {
     public String mostrarUsuarios() throws SQLException {
         ResultadoSQL resultado = GestorDB.getGestorDB().execSELECT("SELECT email FROM Usuario");
         JsonArray lEmailsJSON = new JsonArray();
-        while(resultado.next()){
+        while (resultado.next()) {
             JsonObject json = new JsonObject();
             json.put("email", resultado.getString("email"));
             lEmailsJSON.add(json);
@@ -33,30 +36,38 @@ public class GestorJugadores {
         resultado.close();
         return lEmailsJSON.toJson();
     }
+
     public void eliminar(String pUsuario) throws SQLException {
-        GestorDB.getGestorDB().execSELECT("DELETE FROM Usuario WHERE email = '"+ pUsuario +"'");
+        GestorDB.getGestorDB().execSQL("DELETE FROM Usuario WHERE email = '" + pUsuario + "'");
+        ResultadoSQL resultado = GestorDB.getGestorDB().execSELECT("SELECT contrasena FROM UsuarioEmail WHERE email = '" + pUsuario + "'");
+        if (resultado.next()) {
+            //Si es UsuarioEmail
+            GestorDB.getGestorDB().execSQL("DELETE FROM UsuarioEmail WHERE email = '" + pUsuario + "'");
+        }
         mostrarMensajeEliminar();
     }
-    private void mostrarMensajeEliminar(){
+
+    private void mostrarMensajeEliminar() {
         Alert usuarioEliminado = new Alert(Alert.AlertType.CONFIRMATION);
         usuarioEliminado.setTitle("Usuario eliminado");
         usuarioEliminado.setHeaderText("El usuario ha sido eliminado con éxito");
         usuarioEliminado.show();
     }
 
-    public void guardarUsuarioSeleccionado(String pUsuario){
-            usuarioSeleccionado = pUsuario;
+    public void guardarUsuarioSeleccionado(String pUsuario) {
+        usuarioSeleccionado = pUsuario;
     }
 
     public String getDatosUsuario() throws SQLException {
         ResultadoSQL resultado = GestorDB.getGestorDB().execSELECT("SELECT contrasena FROM UsuarioEmail WHERE email = '" + usuarioSeleccionado + "'");
         JsonObject datos = new JsonObject();
         if (resultado.next()) {
+            //Si es UsuarioEmail
             datos.put("contrasena", resultado.getString("contrasena"));
         }
         ResultadoSQL nivel = GestorDB.getGestorDB().execSELECT("SELECT nivelInicial FROM Usuario WHERE email = '" + usuarioSeleccionado + "'");
-        if (nivel.next()){
-            datos.put("nivel",nivel.getInt("nivelInicial"));
+        if (nivel.next()) {
+            datos.put("nivel", nivel.getInt("nivelInicial"));
         }
         datos.put("email", usuarioSeleccionado);
         nivel.close();
@@ -67,7 +78,7 @@ public class GestorJugadores {
     public String getNiveles() throws SQLException {
         ResultadoSQL resultado = GestorDB.getGestorDB().execSELECT("SELECT nivel FROM Nivel");
         JsonArray lnivelesJSON = new JsonArray();
-        while (resultado.next()){
+        while (resultado.next()) {
             JsonObject json = new JsonObject();
             json.put("nivel", resultado.getString("nivel"));
             lnivelesJSON.add(json);
@@ -77,23 +88,46 @@ public class GestorJugadores {
     }
 
     public void guardar(String pEmail, String pContrasena, int pNivel) throws SQLException {
-        GestorDB.getGestorDB().execSELECT("UPDATE Usuario SET email = '" + pEmail + "', nivelInicial = '" + pNivel + '"');
-        guardarUsuarioSeleccionado(pEmail);
-        if(!pContrasena.isBlank()){
-            ResultadoSQL resultado = GestorDB.getGestorDB().execSELECT("SELECT * FROM UsuarioEmail WHERE email = '" + pEmail + "'");
-            if(resultado.next()){
-                mostrarUsuarioExistente();
-            }
-            else{
-                GestorDB.getGestorDB().execSELECT("UPDATE UsuarioEmail SET email = '" + pEmail + "', contrasena = '" + pContrasena + '"');
+        //Comprobamos que no haya ya un usuario con ese email
+        ResultadoSQL resultado = GestorDB.getGestorDB().execSELECT("SELECT * FROM Usuario WHERE email = '" + pEmail + "'");
+        if (resultado.next()) {
+            mostrarUsuarioExistente();
+        } else {
+            //comprobar que el email introducido es válido
+            Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+            Matcher mather = pattern.matcher(pEmail);
+            if (mather.find() == true) {
+                //Se actualizan los datos en Usuario
+                GestorDB.getGestorDB().execSQL("UPDATE Usuario SET email = '" + pEmail + "', nivelInicial = '" + pNivel + "' WHERE email = '" + usuarioSeleccionado + "'");
+                if (pContrasena != "") {
+                    //Si es UsuarioEmail
+                    GestorDB.getGestorDB().execSQL("UPDATE UsuarioEmail SET email = '" + pEmail + "', contrasena = '" + pContrasena + "' WHERE email = '" + usuarioSeleccionado + "'");
+                }
+                guardarUsuarioSeleccionado(pEmail);
+                mostrarDatosActualizados();
+            } else {
+                Alert emailNoValido = new Alert(Alert.AlertType.INFORMATION);
+                emailNoValido.setTitle("Email no válido");
+                emailNoValido.setHeaderText("Por favor introduzca un email válido. Por ejemplo: ejemplo@email.com");
+                emailNoValido.show();
             }
         }
+        resultado.close();
 
     }
-    private void mostrarUsuarioExistente(){
+
+    private void mostrarUsuarioExistente() {
         Alert usuarioEliminado = new Alert(Alert.AlertType.ERROR);
         usuarioEliminado.setTitle("Ya existe ese usuario");
         usuarioEliminado.setHeaderText("Ya existe un usuario con ese email");
         usuarioEliminado.show();
+    }
+
+    private void mostrarDatosActualizados() {
+        Alert datosActualizados = new Alert(Alert.AlertType.CONFIRMATION);
+        datosActualizados.setTitle("Datos actualizados");
+        datosActualizados.setHeaderText("Los datos se han actualizado con éxito");
+        datosActualizados.show();
     }
 }
